d3ce9b74665ae2dfc2a831ec443c88ccde7bf9af
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-10-29 14:03:41 -0700

    Basic Splash screen and menu working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+     public static MainMenu inst;
+     //name of scene to be loaded
+     public string chosenScene = null;
+
+     //text showing which level is currently selected
+     public Text selectedLevelText;
+     //buttons
+     public Button BuilderButton, startButton;
+
+     private void Awake()
+     {
+          inst = this;
+          DontDestroyOnLoad(transform.gameObject);
+     }
+
+/*     private void Start()
+     {
+          Cursor.lockState = CursorLockMode.None;
+     }
+*/
+
+     //Protein Builder button function
+     public void selectBuilder()
+     {
+          chosenScene = BuilderButton.GetComponentInChildren<Text>().text;
+          selectedLevelText.text = chosenScene;
+     }
+
+
+
+     //start game loads currently selected level
+     //FIXME add function to load desired number of entites
+     public void StartGame()
+     {
+          if (chosenScene == "")
+          {
+               Debug.Log("No Level Selected");
+          }
+          else
+          {
+               //Cursor.lockState = CursorLockMode.Locked;
+               SceneManager.LoadScene(chosenScene);
+          }
+
+     }
+}
---
+++ Assets/Scripts/SplashStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashStart : MonoBehaviour
+{
+     public Button startButton;
+
+     // Start is called before the first frame update
+     void Start()
+     {
+          startButton.onClick.AddListener(LoadMain);
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+
+     public void LoadMain()
+     {
+          SceneManager.LoadScene("MainMenu");
+     }
+}
