096608eed1540fe0d99d615e8fff70f2d490b39a
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-11-05 15:06:40 -0700

    Working on framework for amino acid tracking

---
+++ Assets/AcidMgr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidMgr : MonoBehaviour
+{
+     public static AcidMgr inst;
+     public GameObject[] acids;
+
+
+     // Start is called before the first frame update
+     void Start()
+    {
+          inst = this;
+
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UIMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIMgr : MonoBehaviour
+{
+
+     public static UIMgr inst;
+     public Text[] acidLabels;
+     public Text[] AcidCounts;
+
+
+
+
+
+     // Start is called before the first frame update
+     void Start()
+    {
+          inst = this;
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+     }
+}
