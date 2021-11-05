2b459e445312385f791ac6b176d9c6b5a7e69d10
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-11-05 13:05:29 -0700

    Player movement with mouse

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     public PlayerController mPlayer;
+     public GameObject catcher;
+     public Vector3 position;
+
+     private void Awake()
+     {
+          mPlayer = this;
+     }
+
+     // Start is called before the first frame update
+     void Start()
+    {
+          position = catcher.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          Vector3 mouse = Input.mousePosition;
+          position.x = Camera.main.ScreenToWorldPoint(mouse).x;
+
+          catcher.transform.position = position;
+
+    }
+}
